b997e120ffc1c9921f0a386aadfd2c6a50d0095c
Cinta Satilla <cintasatilla568@gmail.com>
2026-05-24 22:16:39 +0800

    first commit

---
+++ src/main/java/my/company/perpustakan/PerpusTakan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package my.company.perpustakan;
+
+/**
+ *
+ * @author asus
+ */
+public class PerpusTakan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
